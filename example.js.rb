3e1cf4f1ddc0855ecf045fb6d5e0899af2a6907e
lestad <i.am@lestad.net>
2016-09-29 01:08:18 +0300

    Add example

---
+++ example.js.rb
@@ -0,0 +1,24 @@
+import React from 'react'
+import (Component, PropTypes as PT, PureComponent) from 'react'
+import React, (PureComponent, PropTypes as PT) from 'react'
+
+def simple
+end
+
+def bang!
+end
+
+def ask?
+end
+
+def params a,b
+end
+
+def params1(a,b)
+end
+
+def with_bang! a,b
+end
+
+def with_ask?(a,b)
+end
